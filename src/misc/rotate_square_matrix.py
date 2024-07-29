e71d8a174456253737b40b38fcfadb9f2ffdb94f
Boris Mizhen <bmizhen@users.noreply.github.com>
2024-07-29 12:50:53 -0400

    rotate square matrix

---
+++ src/misc/rotate_square_matrix.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+from typing import Any
+
+
+def rotate(matrix: list[list[Any]]) -> list[list[Any]]:
+    matrix.reverse()
+
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix[i])):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    return matrix
+
+
+pprint(rotate([]))
+pprint(rotate([[1]]))
+pprint(rotate([[1, 2], [3, 4]]))
+pprint(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+# 1 2  3 4  3 1
+# 3 4  1 2  4 2
+
+# 1 2 3      7 8 9      7 4 1
+# 4 5 6      4 5 6      8 5 2
+# 7 8 9      1 2 3      9 6 3
